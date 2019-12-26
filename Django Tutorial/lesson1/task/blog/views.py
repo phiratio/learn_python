4b9ec6e67342e678a21ea674ea9b11f8af9b815f
phiratio <phiratio161@gmail.com>
2019-12-26 11:04:17 +0200

    pgp & cleanup

--- Django Tutorial/lesson1/task/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from .models import Post
-
-
-def post_list(request):
-    posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
