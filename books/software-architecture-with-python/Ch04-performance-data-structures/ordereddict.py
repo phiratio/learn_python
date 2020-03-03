bde728760aadff794222d7e0bc0a1bc01f3477ff
phiratio <phiratio161@gmail.com>
2020-03-03 12:45:22 +0200

    refactoring

--- books/software-architecture-with-python/Ch04-performance-data-structures/ordereddict.py
+++ books/software-architecture-with-python/Ch04-performance-data-structures/ordereddict.py
@@ -8,7 +8,7 @@ Examples of using OrderedDict
 
 from collections import OrderedDict
 
-cities = ['Jakarta','Delhi','Newyork','Bonn','Kolkata','Bangalore','Seoul']
+cities = ['Jakarta', 'Delhi', 'Newyork', 'Bonn', 'Kolkata', 'Bangalore', 'Seoul']
 # Dictionary
 cities_dict = dict.fromkeys(cities)
 print(cities_dict)
@@ -17,47 +17,48 @@ ocities_dict = OrderedDict.fromkeys(cities)
 print(ocities_dict)
 
 # Dropping duplicates while preserving order with Ordered dictionary
-cities = ['Jakarta','Delhi','Newyork','Bonn','Kolkata','Bangalore','Bonn','Seoul','Delhi','Jakarta','Mumbai']
+cities = ['Jakarta', 'Delhi', 'Newyork', 'Bonn', 'Kolkata', 'Bangalore', 'Bonn', 'Seoul', 'Delhi', 'Jakarta', 'Mumbai']
 cities_odict = OrderedDict.fromkeys(cities)
 print(cities_odict.keys())
 print(cities_odict.popitem())
-print(cities_odict.popitem(last=False))
+print(cities_odict.popitem())
+
 
 # class LRU
 class LRU(OrderedDict):
     """ Least recently used cache (LRU) dictionary
     using OrderedDict
-    
     """
-    
+
     def __init__(self, size=10):
+        super().__init__()
         self.size = size
 
     def set(self, key):
-        # If key is there delete and reinsert so
-        # it moves to end.
+        # If key is there delete and reinsert so it moves to end.
         if key in self:
             del self[key]
 
         self[key] = 1
-        if len(self)>self.size:
+        if len(self) > self.size:
             # Pop from left
             self.popitem(last=False)
 
+
 if __name__ == "__main__":
-     d=LRU(size=5)
-     d.set('bangalore')
-     d.set('chennai')
-     d.set('mumbai')
-     # Bangalore is set again - moves to right
-     d.set('bangalore')
-     d.set('kolkata')
-     d.set('delhi')
-     # Chennai is set again - so moves to right
-     d.set('chennai')
-    
-     print('Length=>',len(d))
-     print(d)
-     # Kochi is appended and mumbai drops off
-     d.set('kochi')
-     print(d)
+    d = LRU(size=5)
+    d.set('bangalore')
+    d.set('chennai')
+    d.set('mumbai')
+    # Bangalore is set again - moves to right
+    d.set('bangalore')
+    d.set('kolkata')
+    d.set('delhi')
+    # Chennai is set again - so moves to right
+    d.set('chennai')
+
+    print('Length=>', len(d))
+    print(d)
+    # Kochi is appended and mumbai drops off
+    d.set('kochi')
+    print(d)
